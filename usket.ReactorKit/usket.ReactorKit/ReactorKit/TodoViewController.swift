a985b015b9b9392d26ed0b2eeecc7f75985ece2d
9oHigh <usket@icloud.com>
2023-09-20 21:58:53 +0900

    feat: Main 스토리보드 제거 및 SceneDelegate 시작

--- usket.ReactorKit/usket.ReactorKit/ReactorKit/TodoViewController.swift
+++ usket.ReactorKit/usket.ReactorKit/ReactorKit/TodoViewController.swift
@@ -6,14 +6,14 @@
 //
 
 import UIKit
+import ReactorKit
+import RxSwift
 
-class ViewController: UIViewController {
+class TodoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
-
-
 }
 
