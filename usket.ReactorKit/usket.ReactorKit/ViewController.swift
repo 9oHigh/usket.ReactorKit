00c56daefb160e9eaf8b225734bacfd94c29fda3
9oHigh <usket@icloud.com>
2023-09-20 21:45:25 +0900

    add: Reactorkit 추가

---
+++ usket.ReactorKit/usket.ReactorKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  usket.ReactorKit
+//
+//  Created by 이경후 on 2023/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ usket.ReactorKit/usket.ReactorKitUITests/usket_ReactorKitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  usket_ReactorKitUITestsLaunchTests.swift
+//  usket.ReactorKitUITests
+//
+//  Created by 이경후 on 2023/09/20.
+//
+
+import XCTest
+
+final class usket_ReactorKitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
